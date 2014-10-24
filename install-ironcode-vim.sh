e5003682acd2cab922282ebf1b32f78eb8a74cf3
Sal Ferrarello <salcode@salogic.net>
2014-10-24 16:24:38 -0400

    add install file

---
+++ install-ironcode-vim.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -i
+
+# A Quick and Dirty Install file for Ironcode Vim configuration
+
+# make backups of current config
+mv ~/.gvimrc ~/.gvimrc.bkup
+mv ~/.vimrc ~/.vimrc.bkup
+
+# note: if .vim-bkup exists, this puts .vim/ inside it
+mv ~/.vim ~/.vim-bkup
+
+# link to config in ~/ironcode-vim/
+ln -sf ~/ironcode-vim/gvimrc.vim ~/.gvimrc
+ln -sf ~/ironcode-vim/vimrc.vim ~/.vimrc
+ln -sfh ~/ironcode-vim ~/.vim
+
+# install vundle
+git clone https://github.com/gmarik/Vundle.vim ~/.vim/bundle/vundle
+
+# run vim and install plugins
+vim +PluginInstall +qall
